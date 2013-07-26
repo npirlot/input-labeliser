bfb7ea631524f3ba3a49fcf88f30f37542e7da2e
Nicolas <nicolas@pirlotweb.com>
2013-07-26 09:46:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 (function($){
 	$.fn.labelise = function(){
-		var $this = this;
-		console.log($this);
-		console.log(arguments);
+		var $this = this,
+			x = arguments[0];
+			
+		var defaults = {
+			module: "labeliser",
+			author: "Creations NP",
+			version: "1.0",
+			debug: true,
+			delimiter: ",",
+			list: [],	//: { value: "", label: "" }
+			width: "100%",
+			wrapper_class: "",
+			labels_class: ""
+		};
+		
+		if(typeof(arguments[0]) == "object"){
+			x = $.extend(true, {}, defaults, arguments[0]);
+		}
+		
+		x._vals = $this.val().split(x.delimiter);
+		
+		//: set labels:
+		x.labels = []; //: { value: "", label: "", class: "" }
+		
+		
+		if(x.debug == true){
+			console.debug(x);
+		}
+		
+		$this.data("labeliser", {
+			_arguments: x
+		});
+		
 		return $this;
 	};
 }(jQuery));
